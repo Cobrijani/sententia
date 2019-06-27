73835f448725357eae7cd26bb8f6a3a41101ffe8
Stefan Bratic <stefanbraticns@gmail.com>
2019-06-27 11:48:42 +0000

    Generated with JHipster 6.1.1

---
+++ gateway/src/main/java/com/github/cobrijani/gateway/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.github.cobrijani.gateway.config.audit;
---
+++ gateway/src/main/java/com/github/cobrijani/gateway/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.github.cobrijani.gateway.service.dto;
